e7a2f8790a4c6a613567c93a332f8a7c79e334f3
saurabhs <saurabhpshirsath@gmail.com>
2021-07-28 19:26:49 +0530

    [saurabh] Add : Create Contact With Details In AddressBook

---
+++ AddressBookMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBookMain
+    {
+
+        private static List<Person> contacts = new List<Person>();
+        public static void AddMember()
+        {
+            //object for person class
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            person.lastName = Console.ReadLine();
+            Console.Write("Enter Address: ");
+            person.address = Console.ReadLine();
+            Console.Write("Enter City: ");
+            person.city = Console.ReadLine();
+            Console.Write("Enter State: ");
+            person.state = Console.ReadLine();
+            Console.Write("Enter Zip Code: ");
+            person.zipCode = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                Console.Write("Enter Phone Number: ");
+                string phNo = Console.ReadLine();
+                if (phNo.Length == 10)
+                {
+                    person.phoneNumber = phNo;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Phone Number,It should Contain 10 digits");
+                }
+            }
+            while (true)
+            {
+                Console.Write("Enter Email-id: ");
+                string emailId = Console.ReadLine();
+                if (emailId.Contains("@"))
+                {
+                    person.email = emailId;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Email Id, It should Contains @ ");
+                }
+            }
+            contacts.Add(person);
+            Console.WriteLine("Added Successfully");
+        }
+
+        public static void ViewContacts()
+        {
+            if (contacts.Count > 0)
+            {
+                Console.WriteLine("****Your Contact List ****");
+                foreach (var x in contacts)
+                {
+                    PrintValue(x);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Address Book is Empty");
+            }
+        }
+        public static void PrintValue(Person x)
+        {
+            Console.WriteLine($"First Name : {x.firstName}");
+            Console.WriteLine($"Last Name : {x.lastName}");
+            Console.WriteLine($"Address : {x.address}");
+            Console.WriteLine($"City : {x.city}");
+            Console.WriteLine($"State : {x.state}");
+            Console.WriteLine($"Zip Code: {x.zipCode}");
+            Console.WriteLine($"Phone Number: {x.phoneNumber}");
+            Console.WriteLine($"Email: {x.email}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,27 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To AddressBook System Program");
+            while (true)
+            {
+                Console.WriteLine("**************=================****************");
+                Console.WriteLine("Enter First Name ");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name ");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter address ");
+                string address = Console.ReadLine();
+                Console.WriteLine("Enter City: ");
+                string city = Console.ReadLine();
+                Console.WriteLine("Enter State: ");
+                string state = Console.ReadLine();
+                Console.WriteLine("Enter Zip Code: ");
+                int zipCode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Phone Number: ");
+                string phNo = Console.ReadLine();
+                Console.Write("Enter Email-id: ");
+                string emailId = Console.ReadLine();
+                break;
+            }
         }
     }
 }
